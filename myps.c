0aa094829c939cd18dae848d3d5b50c573be39d6
Louis Pottier <lp.louispottier@gmail.com>
2021-12-13 14:21:02 +0100

    Level 1 committed

---
+++ myps.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <string.h>
+
+int error(int nb);
+void parse_argv(char *argv[]);
+void display(char *arg[]);
+
+int main(int argc, const char *argv[])
+{
+    if (strcmp(argv[1], "-o") == 0)
+    {
+        if (argc > 1)
+        {
+            parse_argv(argv);
+        }
+        else
+        {
+            error(1);
+        }
+    }
+
+    return 0;
+}
+
+void parse_argv(char *argv[])
+{
+    char string[] = "pid,ppid,command";
+    char *param;
+
+    param = strtok(argv[2],",");
+
+    while(param)
+    {
+        display(param);
+        param = strtok(NULL,",");
+    }
+    printf("\n");
+}
+
+void display(char *arg[])
+{
+    if (strcmp(arg, "pid") == 0)
+    {
+        printf(" PID ");
+    }
+    else if (strcmp(arg, "ppid") == 0)
+    {
+        printf(" PPID");
+    }
+    else if (strcmp(arg, "command") == 0)
+    {
+        printf(" COMMAND");
+    }
+}
+
+int error(int nb)
+{
+    switch (nb)
+    {
+        case 0:
+            printf("ERROR : wrong extension, you can only use '-o'");
+            break;
+
+        case 1:
+            printf("ERROR : this function must be followed by at least one parameter");
+            break;
+    }
+}
